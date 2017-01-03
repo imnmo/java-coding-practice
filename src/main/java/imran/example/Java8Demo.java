d2d357da8baf5b5686c825898fce9b3f788d44ad
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2017-01-03 17:28:45 +0100

    the right stuff

--- src/main/java/imran/example/Java8Demo.java
+++ src/main/java/imran/example/Java8Demo.java
@@ -1,5 +1,74 @@
 package imran.example;
 
-public class Java8Demo {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Java8Demo {
+	
+	
+	/**
+	 * Sample demo of Java 8 in action with Lambdas and method ref
+	 * and map,reduce, filter, stream
+	 */
+	public void inAction(){
+		final  List<BigDecimal> prices = Arrays.asList(
+				new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+				new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+				new BigDecimal("45"), new BigDecimal("12"));
+		
+		
+		final BigDecimal totalOfDiscountedPrices =
+				prices.stream()
+				.filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+				.map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+		System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
+		
+		//This piece just prints the prices after filtering or mapping 
+		prices.stream()
+		.map(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+		.forEach(price -> System.out.println(price));	
+		
+ 
+	}
+
+	/**
+	 * prints the names  using for each technique
+	 */
+	public void PrintNames()
+	{
+		final List<String> friends =
+				Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");		
+		friends.forEach(name ->System.out.println(name));
+	}
+	
+	
+	/**
+	 * Find a name starting with N and print them l
+	 */
+	public void FindNameWithN()
+	{
+		final List<String> friends =
+				Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");	
+		
+		//filter the "N"
+		final List<String> startWIthN =
+		friends
+		.stream()
+		.filter(name -> (name.startsWith("N")))
+		.collect(Collectors.toList());
+		
+		//print the name with "N"
+		startWIthN
+		.forEach(name ->System.out.println(name));
+	}
+	
+	/**
+	 * 
+	 */
+	
 }
---
+++ src/test/java/imran/example/Java8DemoTest.java
@@ -0,0 +1,18 @@
+package imran.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Java8DemoTest {
+
+	@Test
+	public void test() {
+	
+		Java8Demo myDemo = new Java8Demo();
+		myDemo.inAction();
+		myDemo.PrintNames();
+		myDemo.FindNameWithN();
+	}
+
+}
